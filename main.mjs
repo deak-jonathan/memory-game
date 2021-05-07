b6103511a5223b4c53ee211a4e4d72b3a1cf655e
Deak Jonathan <deakjonathandev@gmail.com>
2021-05-07 08:44:46 +0200

    v1

---
+++ main.mjs
@@ -0,0 +1,118 @@
+import { marvel, starwars, langage, footballTeam, company } from "./theme.mjs";
+
+const cards = document.querySelectorAll(".card");
+let cardReturn = false;
+let firstCard, secondCard;
+let lock = false;
+let win = 0;
+let counter = 0;
+cards.forEach((card) => {
+  card.addEventListener("click", returnCard);
+});
+
+function returnCard() {
+  if (lock) return;
+  this.childNodes[1].classList.toggle("active");
+  if (!cardReturn) {
+    cardReturn = true;
+    firstCard = this;
+    return;
+  }
+  cardReturn = false;
+  secondCard = this;
+  counter++;
+  correspondence();
+  if (win === cards.length / 2) {
+    setTimeout(() => {
+      document.querySelector(".filter").style.display = "flex";
+      document.querySelector(
+        ".round"
+      ).innerHTML = `Nombre d'essais : ${counter}`;
+      document.querySelector(".restart").addEventListener("click", () => {
+        window.location.reload();
+      });
+    }, 650);
+  }
+}
+
+function correspondence() {
+  if (
+    firstCard.getAttribute("data-attr") === secondCard.getAttribute("data-attr")
+  ) {
+    firstCard.removeEventListener("click", returnCard);
+    secondCard.removeEventListener("click", returnCard);
+    win++;
+    console.log(win);
+  } else {
+    lock = true;
+    setTimeout(() => {
+      firstCard.childNodes[1].classList.remove("active");
+      secondCard.childNodes[1].classList.remove("active");
+      lock = false;
+    }, 1300);
+  }
+}
+function random() {
+  cards.forEach((card) => {
+    let randomPos = Math.floor(Math.random() * 16);
+    card.style.order = randomPos;
+  });
+}
+random();
+/***************** ACTION BUTTON THEME******************/
+
+const btnTheme = document.querySelectorAll(".start");
+const imgFace = document.querySelectorAll(".img-face");
+const backFace = document.querySelectorAll(".back-face");
+const title = document.querySelector(".title");
+btnTheme.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    document.querySelector(".start-game").style.display = "none";
+    if (btn.getAttribute("name") === "marvel") {
+      document.body.style.background = marvel[0]["background"];
+      backFace.forEach((face) => {
+        face.style.background = marvel[0]["backgroundCard"];
+      });
+      title.setAttribute("src", "/assets/marvel/marvel.png");
+      addCard(marvel);
+    } else if (btn.getAttribute("name") === "starwars") {
+      document.body.style.background = starwars[0]["background"];
+      backFace.forEach((face) => {
+        face.style.background = starwars[0]["backgroundCard"];
+      });
+      title.setAttribute("src", "/assets/starwars/starwars.png");
+      addCard(starwars);
+    } else if (btn.getAttribute("name") === "langage") {
+      document.body.style.background = langage[0]["background"];
+      backFace.forEach((face) => {
+        face.style.background = langage[0]["backgroundCard"];
+      });
+      title.setAttribute("src", "/assets/langage/code.png");
+      addCard(langage);
+    } else if (btn.getAttribute("name") === "footballTeam") {
+      document.body.style.background = footballTeam[0]["background"];
+      backFace.forEach((face) => {
+        face.style.background = footballTeam[0]["backgroundCard"];
+      });
+      title.setAttribute("src", "/assets/footballTeam/uefa.png");
+      addCard(footballTeam);
+    } else if (btn.getAttribute("name") === "company") {
+      document.body.style.background = company[0]["background"];
+      backFace.forEach((face) => {
+        face.style.background = company[0]["backgroundCard"];
+      });
+      title.setAttribute("src", "/assets/company/gafam.png");
+      addCard(company);
+    }
+  });
+});
+
+function addCard(theme) {
+  let number = 0;
+  for (let j = 1; j <= imgFace.length; j++) {
+    imgFace[number].setAttribute("src", theme[j]["image"]);
+    imgFace[number].setAttribute("alt", theme[j]["data"]);
+    cards[number].setAttribute("data-attr", theme[j]["data"]);
+    number++;
+  }
+}
---
+++ theme.mjs
@@ -0,0 +1,122 @@
+const marvel = [
+  {
+    background:
+      "linear-gradient(90deg, hsla(356, 76%, 50%, 1) 0%, hsla(14, 63%, 36%, 1) 100%)",
+    backgroundCard:
+      "linear-gradient(45deg, hsla(356, 76%, 50%, 1) 0%, hsla(14, 63%, 36%, 1) 100%)",
+  },
+  { image: "/assets/marvel/ironman.png", data: "ironMan" },
+  { image: "/assets/marvel/hulk.png", data: "hulk" },
+  { image: "/assets/marvel/thor.png", data: "thor" },
+  { image: "/assets/marvel/thanos.png", data: "thanos" },
+  { image: "/assets/marvel/groot.png", data: "groot" },
+  { image: "/assets/marvel/blackPanther.png", data: "blackPanther" },
+  { image: "/assets/marvel/captain.png", data: "captainAmerica" },
+  { image: "/assets/marvel/doctorStrange.png", data: "doctorStrange" },
+  { image: "/assets/marvel/ironman.png", data: "ironMan" },
+  { image: "/assets/marvel/hulk.png", data: "hulk" },
+  { image: "/assets/marvel/thor.png", data: "thor" },
+  { image: "/assets/marvel/thanos.png", data: "thanos" },
+  { image: "/assets/marvel/groot.png", data: "groot" },
+  { image: "/assets/marvel/blackPanther.png", data: "blackPanther" },
+  { image: "/assets/marvel/captain.png", data: "captainAmerica" },
+  { image: "/assets/marvel/doctorStrange.png", data: "doctorStrange" },
+];
+const starwars = [
+  {
+    background:
+      " linear-gradient(90deg, hsla(205, 46%, 30%, 1) 0%, hsla(260, 29%, 36%, 1) 100%)",
+    backgroundCard:
+      " linear-gradient(45deg, hsla(205, 46%, 30%, 1) 0%, hsla(260, 29%, 36%, 1) 100%)",
+  },
+  { image: "/assets/starwars/c3po.png", data: "c3po" },
+  { image: "/assets/starwars/kyloren.png", data: "kyloren" },
+  { image: "/assets/starwars/deathstar.png", data: "deathstar" },
+  { image: "/assets/starwars/yoda.png", data: "yoda" },
+  { image: "/assets/starwars/vador.png", data: "vador" },
+  { image: "/assets/starwars/r2d2.png", data: "r2d2" },
+  { image: "/assets/starwars/falcon.png", data: "falcon" },
+  { image: "/assets/starwars/mando.png", data: "mando" },
+  { image: "/assets/starwars/c3po.png", data: "c3po" },
+  { image: "/assets/starwars/kyloren.png", data: "kyloren" },
+  { image: "/assets/starwars/deathstar.png", data: "deathstar" },
+  { image: "/assets/starwars/yoda.png", data: "yoda" },
+  { image: "/assets/starwars/vador.png", data: "vador" },
+  { image: "/assets/starwars/r2d2.png", data: "r2d2" },
+  { image: "/assets/starwars/falcon.png", data: "falcon" },
+  { image: "/assets/starwars/mando.png", data: "mando" },
+];
+const langage = [
+  {
+    background:
+      " linear-gradient(90deg, hsla(59, 86%, 68%, 1) 0%, hsla(134, 36%, 53%, 1) 100%)",
+    backgroundCard:
+      " linear-gradient(45deg, hsla(59, 86%, 68%, 1) 0%, hsla(134, 36%, 53%, 1) 100%)",
+  },
+  { image: "/assets/langage/js.png", data: "js" },
+  { image: "/assets/langage/php.png", data: "php" },
+  { image: "/assets/langage/python.png", data: "python" },
+  { image: "/assets/langage/java.png", data: "java" },
+  { image: "/assets/langage/c++.png", data: "c++" },
+  { image: "/assets/langage/swift.png", data: "swift" },
+  { image: "/assets/langage/dart.png", data: "dart" },
+  { image: "/assets/langage/go.png", data: "go" },
+  { image: "/assets/langage/js.png", data: "js" },
+  { image: "/assets/langage/php.png", data: "php" },
+  { image: "/assets/langage/python.png", data: "python" },
+  { image: "/assets/langage/java.png", data: "java" },
+  { image: "/assets/langage/c++.png", data: "c++" },
+  { image: "/assets/langage/swift.png", data: "swift" },
+  { image: "/assets/langage/dart.png", data: "dart" },
+  { image: "/assets/langage/go.png", data: "go" },
+];
+const footballTeam = [
+  {
+    background:
+      "linear-gradient(90deg, hsla(221, 45%, 73%, 1) 0%, hsla(220, 78%, 29%, 1) 100%)",
+    backgroundCard:
+      "linear-gradient(45deg, hsla(221, 45%, 73%, 1) 0%, hsla(220, 78%, 29%, 1) 100%)",
+  },
+  { image: "/assets/footballTeam/madrid.png", data: "madrid" },
+  { image: "/assets/footballTeam/barca.png", data: "barca" },
+  { image: "/assets/footballTeam/chelsea.png", data: "chelsea" },
+  { image: "/assets/footballTeam/psg.png", data: "psg" },
+  { image: "/assets/footballTeam/city.png", data: "city" },
+  { image: "/assets/footballTeam/mu.png", data: "mu" },
+  { image: "/assets/footballTeam/ajax.png", data: "ajax" },
+  { image: "/assets/footballTeam/bayern.png", data: "bayern" },
+  { image: "/assets/footballTeam/madrid.png", data: "madrid" },
+  { image: "/assets/footballTeam/barca.png", data: "barca" },
+  { image: "/assets/footballTeam/chelsea.png", data: "chelsea" },
+  { image: "/assets/footballTeam/psg.png", data: "psg" },
+  { image: "/assets/footballTeam/city.png", data: "city" },
+  { image: "/assets/footballTeam/mu.png", data: "mu" },
+  { image: "/assets/footballTeam/ajax.png", data: "ajax" },
+  { image: "/assets/footballTeam/bayern.png", data: "bayern" },
+];
+const company = [
+  {
+    background:
+      "linear-gradient(90deg, hsla(259, 84%, 78%, 1) 0%, hsla(206, 67%, 75%, 1) 100%)",
+    backgroundCard:
+      "linear-gradient(45deg, hsla(259, 84%, 78%, 1) 0%, hsla(206, 67%, 75%, 1) 100%)",
+  },
+  { image: "/assets/company/facebook.png", data: "facebook" },
+  { image: "/assets/company/apple.png", data: "apple" },
+  { image: "/assets/company/google.png", data: "google" },
+  { image: "/assets/company/starbucks.png", data: "starbucks" },
+  { image: "/assets/company/microsoft.png", data: "microsoft" },
+  { image: "/assets/company/spotify.png", data: "spotify" },
+  { image: "/assets/company/tesla.png", data: "tesla" },
+  { image: "/assets/company/nike.png", data: "nike" },
+  { image: "/assets/company/facebook.png", data: "facebook" },
+  { image: "/assets/company/apple.png", data: "apple" },
+  { image: "/assets/company/google.png", data: "google" },
+  { image: "/assets/company/starbucks.png", data: "starbucks" },
+  { image: "/assets/company/microsoft.png", data: "microsoft" },
+  { image: "/assets/company/spotify.png", data: "spotify" },
+  { image: "/assets/company/tesla.png", data: "tesla" },
+  { image: "/assets/company/nike.png", data: "nike" },
+];
+
+export { marvel, starwars, langage, footballTeam, company };
